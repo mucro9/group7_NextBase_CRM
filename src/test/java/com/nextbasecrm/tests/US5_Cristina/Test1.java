46cf4a1af25ecc7a11ba46826916a355c6ecdafd
CristinaC <romancristina88@yahoo.com>
2022-02-25 21:58:44 -0500

    Starting the new test

---
+++ src/test/java/com/nextbasecrm/tests/US5_Cristina/Test1.java
@@ -0,0 +1,11 @@
+package com.nextbasecrm.tests.US5_Cristina;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void test(){
+        System.out.println("test");
+    }
+
+}
