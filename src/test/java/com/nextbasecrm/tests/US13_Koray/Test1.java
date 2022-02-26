70803b200e80796e729e86e0472b7af24eb1d498
Koray Basoglu <basoglukoray1@gmail.com>
2022-02-25 20:15:12 -0500

    Test 1

---
+++ src/test/java/com/nextbasecrm/tests/US13_Koray/Test1.java
@@ -0,0 +1,10 @@
+package com.nextbasecrm.tests.US13_Koray;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void test3(){
+        System.out.println("hello");
+    }
+}
