e37094a7fdbfce1aaa165d0e916af29a77da3d8c
Nat <nattayasdet@gmail.com>
2022-02-25 21:05:38 -0500

    initial commit

---
+++ src/test/java/com/nextbasecrm/tests/US7_Nattaya/Test1.java
@@ -0,0 +1,25 @@
+package com.nextbasecrm.tests.US7_Nattaya;
+
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test1 {
+
+    WebDriver driver;
+    @BeforeMethod
+    public void setUp(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+        //
+    }
+    @Test
+    public void test3(){
+        System.out.println("hello");
+    }
+}
