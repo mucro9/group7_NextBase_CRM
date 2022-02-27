96b9ed6141c5dffdb402b105cc16550d19051455
okk33sch <korayozbex@gmail.com>
2022-02-27 13:14:52 -0500

    final commit

--- src/test/java/com/nextbasecrm/tests/US_14_Kozbek/Test1.java
+++ src/test/java/com/nextbasecrm/tests/US_14_Kozbek/Test1.java
@@ -3,6 +3,7 @@ package com.nextbasecrm.tests.US_14_Kozbek;
 import com.nextbasecrm.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class Test1 {
     WebDriver driver;
@@ -13,6 +14,12 @@ public class Test1 {
         driver.manage().window().maximize();
         driver.get("https://apple.com");
         //setup commit
+
+    }
+        @Test
+        public void test1(){
+            System.out.println("Hellow");
+
 
     }
 }
