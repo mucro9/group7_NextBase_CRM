fe9688394737e9d76355a104ba4ecceb5a7272ab
okk33sch <korayozbex@gmail.com>
2022-02-27 13:01:16 -0500

    initial commit

--- src/test/java/com/nextbasecrm/tests/US_14_Kozbek/Test1.java
+++ src/test/java/com/nextbasecrm/tests/US_14_Kozbek/Test1.java
@@ -1,5 +1,18 @@
 package com.nextbasecrm.tests.US_14_Kozbek;
 
-public class Test1 {
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 
+public class Test1 {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("firefox");
+        driver.manage().window().maximize();
+        driver.get("https://apple.com");
+        //setup commit
+
+    }
 }
